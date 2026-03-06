0aeeb3d96b40e24f359173daafd6171d772e93a2
Patrick <108092029+patrickque21@users.noreply.github.com>
2026-03-06 15:38:17 -0300

    aula

---
+++ src/scripts/main.js
@@ -0,0 +1,13 @@
+let menu = document.getElementById('menu');
+let select = document.getElementById('numCards');
+let start = document.getElementById('start')
+
+for(let i=4; i<=10; i+=2){
+    let n = i*i
+    let op = document.createElement('option');
+
+    op.value = n;
+    op.innerHTML = n;
+
+    select.appendChild(op);
+}
